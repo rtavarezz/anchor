1ae67e735df8fec5cf52639a8f3091a435535c7f
mtgnoah <noahpravecek@gmail.com>
2024-07-31 16:17:22 -0400

    new files

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/AnomalyFi/anchor/cli"
+)
+
+func main() {
+	cli.Main()
+}
---
+++ server/backend.go
@@ -0,0 +1,13 @@
+package server
+
+const (
+	// Router paths
+	pathStatus            = "/eth/v1/builder/status"
+	pathRegisterValidator = "/eth/v1/builder/validators"
+	pathGetHeader         = "/eth/v1/builder/header/{slot:[0-9]+}/{parent_hash:0x[a-fA-F0-9]+}/{pubkey:0x[a-fA-F0-9]+}"
+	pathGetPayload        = "/eth/v1/builder/blinded_blocks"
+	pathGetOPPayload        = "/eth/v1/builder/get_payload/{parent_hash:0x[a-fA-F0-9]+}"
+
+	// // Relay Monitor paths
+	// pathAuctionTranscript = "/monitor/v1/transcript"
+)
